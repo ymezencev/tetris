631d70476f9e4fb88f08f56385d2434777f5f3a9
Yuri <yurmezence@gmail.com>
2020-07-29 09:56:54 +0300

    config and style files

---
+++ styles.py
@@ -0,0 +1,16 @@
+project_css = """
+*{
+font-family:century-gothic;
+font-size: 16px;
+background-color: rgb(255, 255, 255);
+}
+
+QPushButton {
+            border: 0px;
+            background: rgb(253, 106, 2);
+            }
+QPushButton:hover {
+            border: 0px;
+            background: rgb(76, 187, 23);
+            }       
+"""
